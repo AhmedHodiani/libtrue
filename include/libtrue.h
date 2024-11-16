d359e420a1348124bab2995b439ff14f1c2cd12b
ahmedhodiani <a.hodiani@hotmail.com>
2024-11-16 09:13:19 +0300

    feat: Define BOOL for compatibility with pre-C99 compilers

--- include/libtrue.h
+++ include/libtrue.h
@@ -6,11 +6,17 @@
 /*   By: ataher <ataher@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 09:09:09 by ataher            #+#    #+#             */
-/*   Updated: 2024/11/16 09:10:27 by ataher           ###   ########.fr       */
+/*   Updated: 2024/11/16 09:11:08 by ataher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBTRUE_H
 # define LIBTRUE_H
+
+# ifndef BOOL
+#  define BOOL int
+#  define TRUE 1
+#  define FALSE 0
+# endif
 
 #endif
